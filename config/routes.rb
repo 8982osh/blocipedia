7817ec7cccaf5f179c064e2ddce779986cd8f7db
Cande Garcia <cruztoyota@gmail.com>
2016-03-02 16:29:55 -0700

    checkpoint 4 wiki crud: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  #get 'wikis/index'
+
+  #get 'wikis/show'
+
+  #get 'wikis/new'
+
+  #get 'wikis/edit'
+
+  resources :wikis
+
   devise_for :users
   get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root to:'welcome#index'
   
