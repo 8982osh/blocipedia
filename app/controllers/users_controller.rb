69870e4d2800c887c5c02d496d43a07952b27dee
Cande Garcia <cruztoyota@gmail.com>
2016-03-30 20:35:48 -0600

    Checkpoint 8 private wikis: completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
 #allow user to change from Premium to Standard membership	
   def downgrade
-  	current_user.update_attribute(:role, 'standard')
+    current_user.update_attribute(:role, 'standard')
+    
+    current_user.wikis.each do |wiki|
+    Wiki.private_wikis(current_user).update_attribute(private :false)
+
   end
 end
 
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,5 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+
+  scope :visisble_to, -> (user) { user ? all : where(public: true) }
 end
